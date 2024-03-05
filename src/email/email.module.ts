1c69a1f10f5332dcd33302279fda55d98f2771ad
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-03-04 23:41:04 -0300

    add emailservice

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,7 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+@Global()
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        service: 'Mail',
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        ignoreTLS: true,
+        secure: false,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      defaults: {
+        from: process.env.FROM_MAIL,
+      },
+    }),
+  ],
   providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {}
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  constructor(private mailerSerivce: MailerService) {}
+
+  async sendEmail(options: { email: string; subject: string; html: string }) {
+    try {
+      const message = {
+        to: options.email,
+        subject: options.subject,
+        html: options.html,
+      };
+
+      const emailSend = await this.mailerSerivce.sendMail({
+        ...message,
+      });
+      return emailSend;
+    } catch (error) {
+      throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
